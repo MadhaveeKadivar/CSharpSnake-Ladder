a4a19c0f2869b4dc93d75d978a1d9b166cb3643c
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-26 09:27:08 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,43 +13,34 @@ namespace SnakeLadderGame
             Console.WriteLine("Welcome to Snake Ladder Game\n");
             Console.WriteLine("Let's Play\n");
             Console.WriteLine("Currently you are at 0 Position\n");
-
             int totalBoardNumber = 100, diceNumber, currentBoardPosition = 0;
-
-
-            Random random = new Random();
-
+            int[] snake = { 1, 3, 5, 67, 87, 92, 95, 98 };  // Creating Array of All Snake Position in board
+            int[] ladder = { 4, 6, 21, 28, 36, 51, 71, 80 };  // Creating Array of All Ladder Position in board
+            Random random = new Random(); // Creating object of Random Class
             while (currentBoardPosition<totalBoardNumber)
             {
-                int[] snake = { 1, 3, 5, 67, 87, 92, 95, 98 };
-                int[] ladder = { 4, 6, 21, 28, 36, 51, 71, 80 };
-                diceNumber = random.Next(1, 7);
+                diceNumber = random.Next(1, 7);  // Generating Random Dice Number between 1 to 6
                 Console.WriteLine($"\n\n\nAfter rolling Dice,The number display on the Dice is : {diceNumber}");
-
                 currentBoardPosition+=diceNumber;
-
                 Console.WriteLine($"\nYou are now at position : {currentBoardPosition}");
 
-                if (Array.Exists(snake, x => x == currentBoardPosition))
+                if (Array.Exists(snake, x => x == currentBoardPosition))  // Checking that current psotion is Top of Snake or not
                 {
                     currentBoardPosition-=diceNumber;
                     Console.WriteLine($"\nYou reached at top of Snake , So You moved {diceNumber} steps behind");
                 }
-                if (Array.Exists(ladder, x => x == currentBoardPosition))
+                if (Array.Exists(ladder, x => x == currentBoardPosition))  // Checking that current psotion is Bottom of Ladder or not
                 {
                     Console.WriteLine($"\nYou reached at bottom of Ladder , So You moved {diceNumber} steps ahead");
                     currentBoardPosition+=diceNumber;
-                }
-               
-                if (currentBoardPosition<0)
+                }               
+                if (currentBoardPosition<0) // If current postion is below 0 then restart wit Position 0
                 {
                     currentBoardPosition=0;
                 }
                 Console.WriteLine($"\nYou are now at position : {currentBoardPosition}");
             }
-            
             Console.ReadLine();
-
         }
     }
 }
