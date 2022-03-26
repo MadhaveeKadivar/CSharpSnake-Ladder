855bf89dce5e2b9cf0c258c73371a8bc7176dbf0
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-03-26 22:52:12 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -13,39 +13,31 @@ namespace SnakeLadderGame
         {
             Console.WriteLine("Welcome to Snake Ladder Game\n");
             Console.WriteLine("Let's Play\n");
-
-
+            //Creating Object for Player 1 and Player 2
             Player player1 = new Player(1);
             Player player2 = new Player(2);
-
             while(true)
             {
-                Console.WriteLine("\n\nPlayer 1 turn's....");
-                player1.DiceRolls();
-                player1.Move();
+                Console.WriteLine("\n\nPlayer 1 turn's...."); 
+                player1.DiceRolls(); // Calling dicerolling Method of Player Class for player 1
+                player1.Move(); // Calling condition checking Method of Player Class for player 1
                 if (player1.currentBoardPosition==Program.totalBoardNumber)
                 {
                     Console.WriteLine("Congratuation ! Player 1 Won" );
                     break;
                 }
                 Console.WriteLine("\n\nPlayer 2 turn's....");
-                player2.DiceRolls();
-                player2.Move();
+                player2.DiceRolls();  // Calling dicerolling Method of Player Class for player 2
+                player2.Move(); // Calling condition checking Method of Player Class for player 2
                 if (player2.currentBoardPosition==Program.totalBoardNumber)
                 {
                     Console.WriteLine("\n\nCongratuation ! Player 2 Won");
                     break;
                 }
-
             }
-
             Console.WriteLine($"\nPlayer 1 rolls dice : {player1.countDiceRolls} times");
             Console.WriteLine($"\nPlayer 2 rolls dice : {player2.countDiceRolls} times");
-
-
             Console.ReadLine();
-
-
         }
     }
 }
